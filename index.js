4b16894e58a2ea63aefe7aaba7ce55a4f019aa11
Aa-sh-sih <aashish33lamsal@gmail.com>
2024-06-08 12:28:32 +0545

    corse

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const dotenv = require('dotenv')
  const mongoose =  require('mongoose')
  const bodyParser = require('body-parser')
-const app = express()
 const port = 8848
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log("db connected")).catch((err)=>console.log(err));
